7ed5e8b70d9a10204399f797a3d8c77cbd8d3a55
Ascialtor <uladzislau.yarmashuk@innowise-group.com>
2023-12-27 18:55:59 +0300

    adsf

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 $(function () {
   function typeWord(string) {
     return new Promise(resolve => {
-      let maxspeed = 600;
+      let maxspeed = 200;
       let currentLetter = 0;
 
       function character(start) {
@@ -10,7 +10,12 @@ $(function () {
 
       function typeLetter() {
         let randomTime = Math.floor(Math.random() * maxspeed);
-        if (currentLetter > string.length) return resolve();
+        if (currentLetter > string.length) {
+          setTimeout(function () {
+            resolve();
+          }, 500);
+          return;
+        }
 
         $('#type').html(
           $('#type').html().slice(0, -14) + character(currentLetter) + `<span>|</span>`
@@ -24,10 +29,15 @@ $(function () {
 
   function removeWord() {
     return new Promise(resolve => {
-      let maxspeed = 600;
+      let maxspeed = 200;
       function removeLetter() {
         let randomTime = Math.floor(Math.random() * maxspeed);
-        if ($('#type').html().length === 14) return resolve();
+        if ($('#type').html().length === 14) {
+          setTimeout(function () {
+            resolve();
+          }, 500);
+          return;
+        }
 
         $('#type').html($('#type').html().slice(0, -15) + `<span>|</span>`);
 
@@ -39,12 +49,16 @@ $(function () {
   }
 
   async function doMagic() {
-    await removeWord();
-    await typeWord('Бара');
-    await removeWord();
-    await typeWord('Доставки');
-    await removeWord();
-    await typeWord('Кофеточки');
+    while (true) {
+      await typeWord('Ресторана');
+      await removeWord();
+      await typeWord('Бара');
+      await removeWord();
+      await typeWord('Доставки');
+      await removeWord();
+      await typeWord('Кофеточки');
+      await removeWord();
+    }
   }
 
   doMagic();
